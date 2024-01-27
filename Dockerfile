d8448f1cfa9f4a1c6f4b42b0abc039e0eb27754c
Gaurav Gupta <gouravgupta840@gmail.com>
2024-01-27 10:53:04 +0530

    [skip ci] added rabbitmq

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ ARG NODE_ENV=development
 ARG ADMIN_PASSWORD=something
 ARG DATABASE=address-table-server
 ARG MONGO_URI=mongodb://mongodb:27017
+ARG RABBIT_MQ=amqp://appname:password@localhost:port
 
 # Please dont change this here, You can change this via outside
 ENV NODE_ENV=${NODE_ENV}
@@ -11,6 +12,7 @@ ENV NODE_ENV=${NODE_ENV}
 # Set environment variables
 ENV ADMIN_PASSWORD=${ADMIN_PASSWORD}
 ENV MONGO_URI=${MONGO_URI}
+ENV RABBIT_MQ=${RABBIT_MQ}
 ENV DATABASE=${DATABASE}
 ENV JWT_KEY=something
 ENV PORT=8080
---
+++ src/mq/rabbitmq-wrapper.ts
@@ -0,0 +1,32 @@
+import * as amqp from 'amqplib';
+
+class RabbitMqWrapper {
+    private _connection: amqp.Connection | null = null;
+
+    get conn() {
+      if (!this._connection) throw new Error("Cannot access MQ before connecting");
+      return this._connection;
+    }
+
+    async connect(url: string) {
+      try {
+        this._connection = await amqp.connect(url);
+        console.log("Connected @", new Date());
+
+        if (this.conn) {
+          // listeners
+        }
+
+        this._connection.on("close", () => {
+            console.error('Connection to RabbitMQ closed. Reconnecting...', new Date());
+            setTimeout(() => this.connect(url), 2000);
+        });
+      } catch (error) {
+        // @ts-ignore
+        console.error(error?.message, new Date());
+        setTimeout(() => this.connect(url), 2000);
+      }
+    }
+}
+
+export const rabbitMqWrapper = new RabbitMqWrapper();
