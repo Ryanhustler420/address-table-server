e33fbf3c09c071414f9ff99e78a92416316d11cc
Gaurav Gupta <gouravgupta840@gmail.com>
2024-02-02 13:34:51 +0530

    [skip ci] refacor the name of inject-env

--- client/src/apis/routes.ts
+++ client/src/apis/routes.ts
@@ -1,21 +1,21 @@
-const host = process.env.REACT_APP_SERVER_HOST_URL || '';
+const backend = process.env.REACT_APP_SERVER_BACKEND_URL || '';
 export const routes = {
-  GET_AUTH_CURRENT_USER: `${host}api/auth/currentuser`,
-  POST_AUTH_REMOVE_ADMIN: `${host}api/auth/remove-admin`,
-  POST_AUTH_MAKE_ADMIN: `${host}api/auth/make-admin`,
-  POST_AUTH_REGISTER: `${host}api/auth/register`,
-  POST_AUTH_LOGOUT: `${host}api/auth/logout`,
-  POST_AUTH_LOGIN: `${host}api/auth/login`,
+  GET_AUTH_CURRENT_USER: `${backend}api/auth/currentuser`,
+  POST_AUTH_REMOVE_ADMIN: `${backend}api/auth/remove-admin`,
+  POST_AUTH_MAKE_ADMIN: `${backend}api/auth/make-admin`,
+  POST_AUTH_REGISTER: `${backend}api/auth/register`,
+  POST_AUTH_LOGOUT: `${backend}api/auth/logout`,
+  POST_AUTH_LOGIN: `${backend}api/auth/login`,
 
-  POST_COMPILERS_CPP: `${host}api/compilers/cpp`,
-  POST_COMPILERS_JAVA: `${host}api/compilers/java`,
-  POST_COMPILERS_NODE: `${host}api/compilers/node`,
-  POST_COMPILERS_PYTHON: `${host}api/compilers/python`,
+  POST_COMPILERS_CPP: `${backend}api/compilers/cpp`,
+  POST_COMPILERS_JAVA: `${backend}api/compilers/java`,
+  POST_COMPILERS_NODE: `${backend}api/compilers/node`,
+  POST_COMPILERS_PYTHON: `${backend}api/compilers/python`,
 
-  GET_RENDER_RENDERS: `${host}api/render`,
-  POST_RENDER_CREATE_RENDER: `${host}api/render`,
-  DELETE_RENDER_CREATE_RENDER: `${host}api/render`,
-  POST_RENDER_PING_RENDERS: `${host}api/render/ping`,
-  GET_RENDER_RENDERS_COUNT: `${host}api/render/tags`,
-  POST_RENDER_REDEPLOY_RENDERS: `${host}api/render/redeploy`,
+  GET_RENDER_RENDERS: `${backend}api/render`,
+  POST_RENDER_CREATE_RENDER: `${backend}api/render`,
+  DELETE_RENDER_CREATE_RENDER: `${backend}api/render`,
+  POST_RENDER_PING_RENDERS: `${backend}api/render/ping`,
+  GET_RENDER_RENDERS_COUNT: `${backend}api/render/tags`,
+  POST_RENDER_REDEPLOY_RENDERS: `${backend}api/render/redeploy`,
 };
